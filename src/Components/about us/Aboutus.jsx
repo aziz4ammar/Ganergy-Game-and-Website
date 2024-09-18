dedfdaa764004fbbd1577323bd7cfad9b277e527
mohamed aziz ammar <azizammar67@gmail.com>
2024-09-18 12:47:58 +0100

    about us page backup

--- src/Components/about us/Aboutus.jsx
+++ src/Components/about us/Aboutus.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import Navbar from '../headder/Navbar'
-// import mlog from "../img/logo copie5 copie3.png";
-// import aziz from "../img/aziz intro.png";
-// import jihed from "../img/jij intro.png";
+import React from 'react'
+import Navbar from '../headder/Navbar'
+import mlog from "../img/logo copie5 copie3.png";
+import aziz from "../img/aziz intro.png";
+import jihed from "../img/jij intro.png";
 
-// const Aboutus = () => {
-//   return (
-//     <div className='big-container-aboutus'>
-//         <Navbar/>
-//         <img id='mi' src={mlog} alt="" />
-//         <div id='aboutus-container'>
-//           <div id='aziz' className='rainbow'><img className='intro' src={aziz} alt="" /></div>
-//           <div className='rainbow'><img className='intro' id='jiji' src={jihed} alt="" /></div>
-//         </div>
-//     </div>
-//   )
-// }
+const Aboutus = () => {
+  return (
+    <div className='big-container-aboutus'>
+        <Navbar/>
+        <img id='mi' src={mlog} alt="" />
+        <div id='aboutus-container'>
+          <div id='aziz' className='rainbow'><img className='intro' src={aziz} alt="" /></div>
+          <div className='rainbow'><img className='intro' id='jiji' src={jihed} alt="" /></div>
+        </div>
+    </div>
+  )
+}
 
-// export default Aboutus
+export default Aboutus
