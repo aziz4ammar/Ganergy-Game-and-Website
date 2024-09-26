0716a813c0a470bc850d9aaa7055ee0e8d8a36fb
mohamed aziz ammar <azizammar67@gmail.com>
2024-09-26 12:55:26 +0100

    test

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -11,12 +11,12 @@ const Main = () => {
   return (
     <div className='m-container'>
         <img id='mi' src={mlog} alt="" />
-        <div id='b1' class="block moving-glow">
+        {/* <div id='b1' class="block moving-glow">
             <div class="line">
                 <img className='garbage' src={garbage} alt="" />
                 <h2 id='mh1' class='lineUp'>Garbage</h2>
             </div>
-        </div>
+        </div> */}
         <div id='b2' class="block moving-glow">
             <div class="line">
                 <img className='gun' src={gun} alt="" />
